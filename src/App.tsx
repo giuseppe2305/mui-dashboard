b88bfd3d583e8d9b2476bc6ca686a0a1a8edcc76
giuseppe2305 <tellihhh@gmail.com>
2025-12-17 15:28:11 +0100

    Extracted Dashboard in its own component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { Box, CssBaseline, Grid, ThemeProvider, Typography, useTheme } from "@mui/material";
-
-import { Sidebar } from "./components/Sidebar";
-import { TransactionsContainer } from "./components/TransactionsContainer";
-import { incomes } from "./mock/incomes";
-import { outcomes } from "./mock/outcomes";
-import { StatisticsContainer } from "./components/StatisticsContainer";
-import { statistics } from "./mock/statistics";
-import { TimeRangeSelector } from "./components/TimeRangeSelector";
-import { ClientsTable } from "./components/ClientsTable";
-import { HeaderBar } from "./components/HeaderBar";
+import { CssBaseline, ThemeProvider, useTheme } from "@mui/material";
+import { Dashboard } from "./components/Dashboard";
 
 function App() {
   const theme = useTheme();
@@ -21,22 +12,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box sx={{ display: "flex", height: "100vh" }}>
-        <Sidebar />
-        <Box component="main" sx={{ flexGrow: "1", overflowY: "auto" }}>
-          <HeaderBar />
-          <Box sx={{ p: 6, display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-            <Typography variant="h4">Dashboard Home</Typography>
-            <TimeRangeSelector />
-          </Box>
-          <Grid container spacing={4} sx={{ pt: 2, pb: 8, px: 12 }}>
-            <StatisticsContainer statisticsData={statistics} />
-            <TransactionsContainer transactions={incomes} label="Incomes History" />
-            <TransactionsContainer transactions={outcomes} label="Outcomes History" />
-            <ClientsTable />
-          </Grid>
-        </Box>
-      </Box>
+      <Dashboard />
     </ThemeProvider>
   );
 }
