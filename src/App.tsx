650f5b9b3223f802be8320ae98fca4ba810889ab
giuseppe2305 <tellihhh@gmail.com>
2025-12-17 11:43:50 +0100

    Built Sidebar.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,66 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { Box, Drawer, Typography } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+
+import HomeIcon from "@mui/icons-material/Home";
+import ShowChart from "@mui/icons-material/ShowChart";
+import Settings from "@mui/icons-material/Settings";
+import Person from "@mui/icons-material/Person";
 
 function App() {
   return (
     <Box sx={{ display: "flex" }}>
-      <Drawer variant="permanent" sx={{ p: 4 }}>
-        <Typography>MUI Dashboard</Typography>
+      <Drawer variant="permanent">
+        <Grid sx={{ p: 4, flexDirection: "column" }}>
+          <Typography variant="h6" sx={{ textAlign: "center" }}>
+            MUI Dashboard
+          </Typography>
+          <List>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <ShowChart />
+                </ListItemIcon>
+                <ListItemText primary="Charts" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Settings />
+                </ListItemIcon>
+                <ListItemText primary="Settings" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Person />
+                </ListItemIcon>
+                <ListItemText primary="Account" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Grid>
       </Drawer>
       <Box component="main" sx={{ flexGrow: "1", bgcolor: "red" }}></Box>
     </Box>
