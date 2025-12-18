fecc81b2ac1a50ef16446855c368aa1296d4f56f
giuseppe2305 <tellihhh@gmail.com>
2025-12-18 18:01:00 +0100

    Implemented a fixed box to prevent animations glitch.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,25 @@
 import { Sidebar } from "./Sidebar";
 import { Home } from "./Home";
 
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { SidebarProvider } from "../contexts/SidebarContext";
 
 function Dashboard() {
   return (
     <SidebarProvider>
       <Stack direction="row" sx={{ height: "100vh" }}>
+        <Box
+          sx={{
+            height: 56,
+            width: "100%",
+            backgroundColor: "primary.main",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        ></Box>
+        {/* Review this part */}
         <Sidebar />
         <Home />
       </Stack>
