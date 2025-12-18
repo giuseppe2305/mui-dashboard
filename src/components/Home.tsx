781cc8d4c6b312126aa597244dfce722fcb6ccae
giuseppe2305 <tellihhh@gmail.com>
2025-12-18 11:07:05 +0100

    Implemented responsive design for Dashbord Heading.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,16 +8,24 @@ import { StatisticsContainer } from "./StatisticsContainer";
 import { TimeRangeSelector } from "./TimeRangeSelector";
 import { TransactionsContainer } from "./TransactionsContainer";
 
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography, Grid, Stack } from "@mui/material";
 
 function Home() {
   return (
     <Box component="main" sx={{ flexGrow: "1", overflowY: "auto" }}>
       <HeaderBar />
-      <Box sx={{ p: 6, display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+      <Stack
+        sx={{
+          p: 6,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexDirection: { md: "row" },
+        }}
+      >
         <Typography variant="h4">Dashboard Home</Typography>
         <TimeRangeSelector />
-      </Box>
+      </Stack>
       <Grid container spacing={4} sx={{ pt: 2, pb: 8, px: 12, justifyContent: "center" }}>
         <StatisticsContainer statisticsData={statistics} />
         <TransactionsContainer transactions={incomes} label="Incomes History" />
