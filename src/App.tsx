eba784110c657d9f132ae33ca2da3fce23890521
giuseppe2305 <tellihhh@gmail.com>
2025-12-19 11:20:01 +0100

    Added sidebar in all routes.

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,29 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Dashboard } from "./components/Dashboard";
+import { CssBaseline, Stack, ThemeProvider } from "@mui/material";
 import theme from "./lib/theme";
 
 import { BrowserRouter, Route, Routes } from "react-router";
+import { SidebarProvider } from "./contexts/SidebarContext";
+import { SidebarBackgroundFix } from "./components/SidebarBackgroundPlaceholder";
+import { Sidebar } from "./components/Sidebar";
+import { Home } from "./components/Home";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
+        <SidebarProvider>
+          <Stack direction="row" sx={{ height: "100vh" }}>
+            <SidebarBackgroundFix />
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Stack>
+        </SidebarProvider>
       </BrowserRouter>
     </ThemeProvider>
   );
