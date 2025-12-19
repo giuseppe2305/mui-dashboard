4d8ceaba8a6371b5a01568d99f35f67f1232d0cc
giuseppe2305 <tellihhh@gmail.com>
2025-12-19 16:12:34 +0100

    Edited ThemeContext to manage theme change.

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,17 +1,30 @@
 import { createContext, useState } from "react";
+import { ThemeProvider as MUIThemeProvider } from "@mui/material";
+
 import type { ChildrenNode } from "../types/ChildrenNode";
+import type { Theme } from "@mui/material";
+
+import { makeTheme } from "../lib/makeTheme";
+import type { paletteColor } from "../lib/palettes";
 
 interface ContextValue {
-  theme: string;
-  setTheme: React.Dispatch<React.SetStateAction<string>>;
+  theme: Theme;
+  themeName: paletteColor;
+
+  setThemeName: (color: paletteColor) => void;
 }
 
 const ThemeContext = createContext<undefined | ContextValue>(undefined);
 
 function ThemeProvider({ children }: ChildrenNode) {
-  const [theme, setTheme] = useState("blue");
+  const [themeName, setThemeName] = useState<paletteColor>("blue");
+  const theme = makeTheme(themeName);
 
-  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
+  return (
+    <MUIThemeProvider theme={theme}>
+      <ThemeContext.Provider value={{ theme, themeName, setThemeName }}>{children}</ThemeContext.Provider>
+    </MUIThemeProvider>
+  );
 }
 
 export { ThemeProvider, ThemeContext };
