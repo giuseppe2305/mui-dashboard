fcc8d108e6fe3217379fe8aa527f9d6a80089e17
giuseppe2305 <tellihhh@gmail.com>
2025-12-19 16:06:32 +0100

    Extracted palettes in their own file.

---
+++ src/lib/palettes.ts
@@ -0,0 +1,28 @@
+export const palettes = {
+  blue: {
+    color: "#0f62fe",
+    textColor: "#ffffff",
+  },
+  red: {
+    color: "#d32f2f",
+    textColor: "#ffffff",
+  },
+  green: {
+    color: "#00a676",
+    textColor: "#ffffff",
+  },
+  purple: {
+    color: "#6a1b9a",
+    textColor: "#ffffff",
+  },
+  orange: {
+    color: "#a1661eff",
+    textColor: "#ffffff",
+  },
+  teal: {
+    color: "#00796b",
+    textColor: "#ffffff",
+  },
+};
+
+export type paletteColor = keyof typeof palettes;
