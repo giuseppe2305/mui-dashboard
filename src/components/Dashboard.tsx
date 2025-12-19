ddd459f1084ca5493f4839927ab121ca18e24ab7
giuseppe2305 <tellihhh@gmail.com>
2025-12-19 09:29:31 +0100

    Exported sidebar background fix in its own component.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,24 +1,15 @@
 import { Sidebar } from "./Sidebar";
 import { Home } from "./Home";
 
-import { Box, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { SidebarProvider } from "../contexts/SidebarContext";
-import { HEADBAR_HEIGHT, SIDEBAR_WIDTH } from "../lib/constants";
+import { SidebarBackgroundFix } from "./SidebarBackgroundPlaceholder";
 
 function Dashboard() {
   return (
     <SidebarProvider>
       <Stack direction="row" sx={{ height: "100vh" }}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            backgroundColor: "primary.main",
-            width: SIDEBAR_WIDTH,
-            height: HEADBAR_HEIGHT,
-          }}
-        ></Box>
+        <SidebarBackgroundFix />
         <Sidebar />
         <Home />
       </Stack>
---
+++ src/components/SidebarBackgroundPlaceholder.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import { HEADBAR_HEIGHT, SIDEBAR_WIDTH } from "../lib/constants";
+
+function SidebarBackgroundFix() {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        backgroundColor: "primary.main",
+        width: SIDEBAR_WIDTH,
+        height: HEADBAR_HEIGHT,
+      }}
+    ></Box>
+  );
+}
+
+export { SidebarBackgroundFix };
