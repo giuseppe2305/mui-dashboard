80adc3c0f80dc576156e60629293090184f8f682
crstelli <crstelli@gmail.com>
2025-12-22 10:52:43 +0100

    Added ThemeSwitch in settings page.

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,10 +1,13 @@
 import { PageContainer } from "./PageContainer";
+import { ThemeSwitch } from "./ThemeSwitch";
 
 function Settings() {
   return (
     <PageContainer>
       <PageContainer.Title>Settings</PageContainer.Title>
       <PageContainer.Subtitle>Manage settings of the app and adjust your own preferences</PageContainer.Subtitle>
+
+      <ThemeSwitch />
     </PageContainer>
   );
 }
